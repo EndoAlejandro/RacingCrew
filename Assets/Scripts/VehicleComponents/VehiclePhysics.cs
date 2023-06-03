3ea4b9d65010bb7d206290d8874247465e007a5b
Alejandro Endo <119749698+EndoAlejandro@users.noreply.github.com>
2023-06-03 11:37:50 +0200

    steering grip and suspension working

--- Assets/Scripts/VehicleComponents/VehiclePhysics.cs
+++ Assets/Scripts/VehicleComponents/VehiclePhysics.cs
@@ -13,23 +13,41 @@ namespace VehicleComponents
 
         [Header("Suspension")]
         [SerializeField] private float suspensionDistance = 2f;
+
         [SerializeField] private float springStrength = 100f;
         [SerializeField] private float damp = 1f;
-        
-        // [Header("Steering")]
-        // [SerializeField] private float 
+
+        [Header("Steering")]
+        [Range(0f, 60f)]
+        [SerializeField] private float steerAngle = 45f;
+
+        [SerializeField] private float tireGrip;
+
+        [SerializeField] private float tireMass;
+
+        [Header("Acceleration")]
+        [SerializeField] private float acceleration;
+
+        [SerializeField] private float maxSpeed;
+
+        [SerializeField] private AnimationCurve accelerationCurve;
+
 
         public float SuspensionDistance => suspensionDistance;
         public float SpringStrength => springStrength;
         public float Damp => damp;
+        public float TireGrip => tireGrip;
+        public float TireMass => tireMass;
+        public float MaxSpeed => maxSpeed;
+        public float Acceleration => acceleration;
+        public float SteerAngle => steerAngle;
+        public AnimationCurve AccelerationCurve => accelerationCurve;
         public Rigidbody Rigidbody { get; private set; }
+
         private void Awake() => Rigidbody = GetComponent<Rigidbody>();
 
         private void FixedUpdate()
         {
-            var vertical = Input.GetAxisRaw("Vertical") * speed;
-            var horizontal = Input.GetAxisRaw("Horizontal") * steering;
-
             /*Rigidbody.AddForceAtPosition(new Vector3(horizontal, 0f, vertical),
                 transform.position + transform.forward * -2f);*/
         }
--- Assets/Scripts/VehicleComponents/WheelPhysics.cs
+++ Assets/Scripts/VehicleComponents/WheelPhysics.cs
@@ -1,14 +1,18 @@
-using System;
 using UnityEngine;
 
 namespace VehicleComponents
 {
     public class WheelPhysics : MonoBehaviour
     {
+        [SerializeField] private bool canSteer;
+
         private VehiclePhysics _vehiclePhysics;
 
+        private Vector3 _suspensionForce;
+        private Vector3 _steeringForce;
+        private Vector3 _accelerationForce;
+
         private RaycastHit _hit;
-        private Vector3 _force;
 
         private void Awake()
         {
@@ -16,29 +20,76 @@ namespace VehicleComponents
             _hit = new RaycastHit();
         }
 
+        private void Update()
+        {
+            if (!canSteer) return;
+
+            var horizontal = Input.GetAxisRaw("Horizontal");
+            var angle = _vehiclePhysics.SteerAngle * horizontal;
+
+            transform.localRotation = Quaternion.Euler(new Vector3(0f, angle, 0f));
+        }
+
         private void FixedUpdate()
         {
             if (!Physics.Raycast(transform.position, -_vehiclePhysics.transform.up, out _hit,
                     _vehiclePhysics.SuspensionDistance)) return;
-            
+
+            Acceleration();
+            var worldVelocity = _vehiclePhysics.Rigidbody.GetPointVelocity(transform.position);
+            Grip(worldVelocity);
+            Suspension(worldVelocity);
+        }
+
+        private void Acceleration()
+        {
+            var vertical = Input.GetAxisRaw("Vertical");
+
+            // if (!(vertical > 0f)) return;
+            var accelerationDirection = transform.forward;
+            var speed = Vector3.Dot(_vehiclePhysics.transform.forward, _vehiclePhysics.Rigidbody.velocity);
+            var normalizedSpeed = Mathf.Clamp01(Mathf.Abs(speed) / _vehiclePhysics.MaxSpeed);
+
+            var torque = _vehiclePhysics.AccelerationCurve.Evaluate(normalizedSpeed) * vertical;
+            _accelerationForce = accelerationDirection * (torque * _vehiclePhysics.Acceleration);
+
+            _vehiclePhysics.Rigidbody.AddForceAtPosition(_accelerationForce, transform.position);
+        }
+
+        private void Grip(Vector3 worldVelocity)
+        {
+            var steeringDirection = transform.right;
+            var steeringVelocity = Vector3.Dot(steeringDirection, worldVelocity);
+
+            var desiredVelocityChange = -steeringVelocity * _vehiclePhysics.TireGrip;
+            var desiredAcceleration = desiredVelocityChange / Time.fixedDeltaTime;
+
+            _steeringForce = steeringDirection * _vehiclePhysics.TireMass * desiredAcceleration;
+            _vehiclePhysics.Rigidbody.AddForceAtPosition(_steeringForce, transform.position);
+        }
+
+        private void Suspension(Vector3 worldVelocity)
+        {
             var springDirection = transform.up;
-
-            // Get velocity of a point relative to rigidbody.
-            var tireWorldVelocity = _vehiclePhysics.Rigidbody.GetPointVelocity(transform.position);
-
             var offset = _vehiclePhysics.SuspensionDistance - _hit.distance;
 
-            var velocity = Vector3.Dot(springDirection, tireWorldVelocity);
+            var velocity = Vector3.Dot(springDirection, worldVelocity);
             var force = (offset * _vehiclePhysics.SpringStrength) - (velocity * _vehiclePhysics.Damp);
 
-            _force = springDirection * force;
-            _vehiclePhysics.Rigidbody.AddForceAtPosition(_force, transform.position);
+            _suspensionForce = springDirection * force;
+            _vehiclePhysics.Rigidbody.AddForceAtPosition(_suspensionForce, transform.position);
         }
 
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.green;
-            Gizmos.DrawLine(transform.position, transform.position + _force);
+            Gizmos.DrawLine(transform.position, transform.position + _suspensionForce);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + _steeringForce);
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, transform.position + _accelerationForce);
         }
     }
 }
