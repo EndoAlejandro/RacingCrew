0b688dae6dd7ec63c585b7f85cc28c7d4289c1d2
Alejandro Endo <lordendo96@gmail.com>
2023-06-13 22:28:09 +0200

    sound manager base implemented

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
+using CustomUtils;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : Singleton<SoundManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Audio Source")]
+    [SerializeField] private AudioSource musicAudioSource;
+
+    [SerializeField] private AudioSource fxAudioSource;
+
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip uiAudioClip;
+
+    private Dictionary<Sfx, AudioClip> _audioClips;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+
+        _audioClips = new Dictionary<Sfx, AudioClip>
+        {
+            { Sfx.UI, uiAudioClip }, // This is where we add all the Sfx enum cases.
+        };
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // This is the method that we can call from any class.
+    public void PlayFx(Sfx fxType) => fxAudioSource.PlayOneShot(_audioClips[fxType]);
+}
+
+// Add as many as you need to ID different audio cases.
+public enum Sfx
+{
+    UI,
 }
